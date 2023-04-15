f27507012a233e368776452e314211538fcd2864
Sergei <67122192+djocson@users.noreply.github.com>
2023-04-15 23:01:42 +0300

    Update dbo.TR_Basket_insert_update.sql

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -2,14 +2,16 @@ create trigger dbo.TR_Basket_insert_update
     on dbo.Basket
 after insert
     as begin
+        with mpl as (
+            select ID_SKU, 
+                case when (count(ID) > 1) then 0.05
+                else 0 end as dcnt
+            from inserted
+            group by ID_SKU
+        )
         update dbo.Basket
         set DiscountValue =  i.Value * dcnt
         from Basket as b
             join inserted as i on b.ID = i.ID
-                join (select ID_SKU, 
-                        case when (count(ID) > 1) then 0.05
-                        else 0 end as dcnt
-                    from inserted
-                    group by ID_SKU
-                ) as c on i.ID_SKU = c.ID_SKU
+                join mpl on i.ID_SKU = mpl.ID_SKU
     end;
