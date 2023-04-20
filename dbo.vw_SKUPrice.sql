52106ab5b5dfcfefc2d4a1459139ccb12e4f086f
Sergei <67122192+djocson@users.noreply.github.com>
2023-04-20 09:00:43 +0300

    Update dbo.vw_SKUPrice.sql

--- dbo.vw_SKUPrice.sql
+++ dbo.vw_SKUPrice.sql
@@ -1,6 +1,6 @@
 create view dbo.vw_SKUPrice as 
-    select ID
-        , Code
-        , Name
-        , dbo.udf_GetSKUPrice(ID) as Price
+    select ID, 
+        Code, 
+        Name, 
+        dbo.udf_GetSKUPrice(ID) as Price
     from dbo.SKU;
