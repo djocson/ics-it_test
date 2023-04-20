e55ca0d79560cd0df73a1c1ac57148e7f4df1a62
Sergei <67122192+djocson@users.noreply.github.com>
2023-04-20 09:04:17 +0300

    Update dbo.udf_GetSKUPrice.sql

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -2,7 +2,6 @@ create function dbo.udf_GetSKUPrice (@sku_id as int)
 returns decimal(18, 2)
 begin
     declare @price as decimal(18, 2)
-    
     set @price = (select sum(Value) / sum (Quantity)
         from dbo.Basket
         group by ID_SKU
