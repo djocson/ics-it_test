7bd2b85fa58e8dacce8f47bf4618432d321f2632
Sergei <67122192+djocson@users.noreply.github.com>
2023-04-20 09:03:09 +0300

    Update dbo.usp_MakeFamilyPurchase.sql

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -1,10 +1,12 @@
 create procedure dbo.usp_MakeFamilyPurchase (@FamilySurName as varchar(255)) as
 begin try
-	if @FamilySurName not in (select SurName from dbo.Family) throw 50000, 'error, there is no such surname in the database', 1;
+	if @FamilySurName not in (select SurName from dbo.Family) 
+		throw 50000, 'error, there is no such surname in the database', 1;
 	else
 		update dbo.Family
     	set BudgetValue = BudgetValue - (select sum(Value)
-        	from dbo.Basket as b join dbo.Family as f on b.ID_Family = f.ID
+        	from dbo.Basket as b 
+		join dbo.Family as f on b.ID_Family = f.ID
         	where f.SurName = @FamilySurName
         	group by ID_Family
         	-- having f.SurName = @FamilySurName
