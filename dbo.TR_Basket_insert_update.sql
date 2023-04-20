69482267e1b62191f55facb344e6433ed502f9f7
Sergei <67122192+djocson@users.noreply.github.com>
2023-04-20 09:00:00 +0300

    Update dbo.TR_Basket_insert_update.sql

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -2,8 +2,7 @@ create trigger dbo.TR_Basket_insert_update on dbo.Basket
 after insert as
 begin
     with mpl as (select ID_SKU, 
-            case when (count(ID) > 1) then 0.05
-            else 0 end as dcnt
+            case when (count(ID) > 1) then 0.05 else 0 end as dcnt
         from inserted
         group by ID_SKU)
     update dbo.Basket
